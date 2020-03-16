cf22ffae06ebc1984dc61a190d68c47f7c01cdc1
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-16 10:12:00 -0400

    Finished histogram

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const buildHistogram = (magazine) => {
+
+    let histogram = {};
+    for(let i=0; i<magazine.length; i++){
+        if (Object.keys(histogram).includes(magazine[i])){
+            histogram[magazine[i]] += 1
+        }
+        else{
+            histogram[magazine[i]] = 1
+        }
+    }
+
+    return histogram
+}
